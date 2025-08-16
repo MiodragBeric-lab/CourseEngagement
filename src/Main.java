e505995f4728a5635cd2f69541bf61756fad012b
itjoke88 <66725815+itjoke88@users.noreply.github.com>
2025-08-16 14:38:38 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.stream.Stream;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Course pmc  = new Course("PMC", "Python Masterclass");
+        Course jmc = new Course("JMC","Java masterclass");
+//        Student tim  = new Student("AU", 2019,30,"M", true, pmc,jmc);
+//        System.out.println(tim);
+//
+//        tim.watchLecture("JMC",10,5,2019);
+//        tim.watchLecture("PMC",7,7,2020);
+//        System.out.println(tim);
+
+//        Stream.generate(() -> Student.getRandomStudent(jmc,pmc))
+//                .limit(10)
+//                .forEach(System.out::println);
+
+
+
+
+
+
+    }
+
+
+
+
+}
